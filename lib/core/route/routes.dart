a7fbbcc37871aa8f62c98d57d5d3e985582eb1b1
abdullahshahoud <abdullahshahoud9@gmail.com>
2026-05-04 13:38:03 +0300

    Add initial app structure with routing and theming support

---
+++ lib/core/route/routes.dart
@@ -0,0 +1,17 @@
+class Routes {
+  // Startup
+  static const String startupError = '/startup-error';
+
+  // Onboarding
+  static const String onboardingScreen = '/onboarding';
+
+  // Auth Routes
+  static const String loginScreen = '/login';
+  static const String registerScreen = '/register';
+  static const String forgotPasswordScreen = '/forgotPassword';
+  static const String resetPasswordScreen = '/resetPassword';
+  static const String verifyEmailScreen = '/verifyEmail';
+
+  // Home
+  static const String homeScreen = '/home';
+}
